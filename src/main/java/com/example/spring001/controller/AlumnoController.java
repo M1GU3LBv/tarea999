3556bfa01cfaba5cba5a88f9a51341b36236a8fe
dreyna <reyna_b@upeu.edu.pe>
2022-06-23 11:06:51 -0500

    V1

---
+++ src/main/java/com/example/spring001/controller/AlumnoController.java
@@ -0,0 +1,24 @@
+
+package com.example.spring001.controller;
+
+import com.example.spring001.service.AlumnoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+public class AlumnoController {
+    @Autowired
+    private AlumnoService alumnoService;
+    
+    @GetMapping
+    public String listarAlumno(Model model){
+        model.addAttribute("alumnos", alumnoService.readAll());
+        return "index";
+    } 
+}
